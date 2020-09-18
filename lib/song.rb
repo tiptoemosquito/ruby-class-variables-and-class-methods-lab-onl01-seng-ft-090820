f465dfa8bc923c36b60f12b03034934ccccf9cbf
Christina Howard <clhoward33@icloud.com>
2020-09-18 17:47:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  def song_count=(count)
+    @song_count = count
+  end
+  def song_count
+    @song_count
+  end
+
+  def song_artists=(artists)
+    @song_artists = artists
+  end
+  def song_artists
+    @song_artists
+  end
+
+  def song_genres=(genres)
+    @song_genres = genres
+  end
+  def song_genres
+    @song_genres
+  end
+
+
+
+end
