74248407241b7c6bca8504b6baae8444b7c2e7f5
Christina Howard <clhoward33@icloud.com>
2020-09-18 18:04:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,29 @@
 class Song
-  attr_accessor :genre, :name, :artist
-    @@count = 0
-    @@genres = []
-    @@artists = []
+  attr_accessor :name, :artist, :genre
 
-    def initialize(name, genre, artist)
-      @name = name
-      @artist = artist
-      @genre = genre
-      @@count += 1
-      @@genres << genre
-      @@artists << artist
-    end
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
-    def self.count
-      @@count
-    end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
 
-    def self.artists
-      @@artists.uniq
-    end
+  def self.count
+    @@count
+  end
 
-    def self.genres
-      @@genres.uniq
-    end
+  def self.artists
+    @@artists.uniq
+  end
 
-
-
+  def self.genres
+     @@genres.uniq
+   end
 
 end
