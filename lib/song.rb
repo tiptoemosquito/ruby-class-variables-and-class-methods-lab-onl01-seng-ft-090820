468fca7500106f025a5db1bb7f590c19e787df0a
Christina Howard <clhoward33@icloud.com>
2020-09-18 18:09:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,22 @@ class Song
     @@artists.uniq
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count
+      end
+    end
+    artist_count
+  end
+
   def self.genres
      @@genres.uniq
    end
+
+  
 
 end
